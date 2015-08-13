3af3ab36bbd9727ce801992a10ac813642f15fd5
Zach <zackify@gmail.com>
2015-08-12 23:22:17 -0700

    added shortcut to turn off lights

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 var app = require('app')
 var Hue = require('./hue')
 var Tray = require('tray')
+var globalShortcut = require('global-shortcut');
 
 var hue = Hue()
 app.on('ready', function() {
   appIcon = new Tray('./icon.png');
   hue.renderMenu()
+  globalShortcut.register('cmd+h', function() {
+    hue.allLights()
+  })
 })
 
 app.dock.hide()
